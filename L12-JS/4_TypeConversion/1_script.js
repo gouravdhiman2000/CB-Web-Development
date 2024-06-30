6710819cf9bc382cafc1f3ac10f6cc7c758913c3
gouravdhiman2000 <gourav.dhiman331@gmail.com>
2024-06-30 13:32:22 +0530

    Delete L12-JS directory

--- L12-JS/4_TypeConversion/1_script.js
+++
@@ -1,7 +0,0 @@
-console.log("0 == \\t", 0 == '\t'); //true - because of type conversion
-console.log("0 == \\n", 0 == '\n'); //true - because of type conversion
-console.log("\\t == \\n", '\t'== '\n'); //false - because of no type conversion
-
-
-//to convert a value to a Number we can use + sign before it
-console.log('Converting +\\t to Number :',+'\t');
