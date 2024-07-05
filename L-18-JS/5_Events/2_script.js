fa0553996dd2ab4c11ad4deb253902091ac74dd5
gouravdhiman2000 <gourav.dhiman331@gmail.com>
2024-07-05 16:54:10 +0530

    learn different concept of event handling in JS

---
+++ L-18-JS/5_Events/2_script.js
@@ -0,0 +1,11 @@
+let para = document.querySelector('.para');
+let btn = document.querySelector('button');
+
+btn.addEventListener('click',()=>{
+    console.log("Clicked Me");
+})
+
+
+btn.addEventListener('click',()=>{
+    console.log("I am Clicked again");
+})
